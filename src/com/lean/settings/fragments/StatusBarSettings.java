ce6f70291bc74c765867ae4d7f32c6d27d713626
Simrat Singh <simrats169169@gmail.com>
2018-11-25 21:56:52 +0530

    partially fix the god awful indentation

--- src/com/lean/settings/fragments/StatusBarSettings.java
+++ src/com/lean/settings/fragments/StatusBarSettings.java
@@ -1,6 +1,7 @@
 package com.lean.settings.fragments;
- import com.android.internal.logging.nano.MetricsProto;
- import android.os.Bundle;
+
+import com.android.internal.logging.nano.MetricsProto;
+import android.os.Bundle;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
@@ -17,7 +18,7 @@ import android.support.v14.preference.PreferenceFragment;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
 import com.android.settings.R;
- import java.util.Locale;
+import java.util.Locale;
 import android.text.TextUtils;
 import android.view.View;
 
@@ -26,40 +27,41 @@ import com.lean.settings.preferences.CustomSeekBarPreference;
 import com.lean.settings.preferences.SystemSettingSwitchPreference;
 import com.android.settings.Utils;
 import android.util.Log;
- import java.util.List;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Collections;
- public class StatusBarSettings extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
+
+    public class StatusBarSettings extends SettingsPreferenceFragment implements
+          OnPreferenceChangeListener {
 
     private CustomSeekBarPreference mThreshold;
     private SystemSettingSwitchPreference mNetMonitor;
 
-     @Override
+    @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-         addPreferencesFromResource(R.xml.lean_settings_statusbar);
-         PreferenceScreen prefSet = getPreferenceScreen();
+        addPreferencesFromResource(R.xml.lean_settings_statusbar);
+        PreferenceScreen prefSet = getPreferenceScreen();
 
         final ContentResolver resolver = getActivity().getContentResolver();
-         boolean isNetMonitorEnabled = Settings.System.getIntForUser(resolver,
+        boolean isNetMonitorEnabled = Settings.System.getIntForUser(resolver,
                 Settings.System.NETWORK_TRAFFIC_STATE, 1, UserHandle.USER_CURRENT) == 1;
         mNetMonitor = (SystemSettingSwitchPreference) findPreference("network_traffic_state");
         mNetMonitor.setChecked(isNetMonitorEnabled);
         mNetMonitor.setOnPreferenceChangeListener(this);
 
-         int value = Settings.System.getIntForUser(resolver,
-                Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, 1, UserHandle.USER_CURRENT);
+        int value = Settings.System.getIntForUser(resolver,
+               Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, 1, UserHandle.USER_CURRENT);
         mThreshold = (CustomSeekBarPreference) findPreference("network_traffic_autohide_threshold");
         mThreshold.setValue(value);
         mThreshold.setOnPreferenceChangeListener(this);
         mThreshold.setEnabled(isNetMonitorEnabled);
      }
-     @Override
+     
+    @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-
         if (preference == mNetMonitor) {
             boolean value = (Boolean) objValue;
             Settings.System.putIntForUser(getActivity().getContentResolver(),
@@ -78,7 +80,7 @@ import java.util.Collections;
          return false;
     }
      @Override
-    public int getMetricsCategory() {
+     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.LEAN;
     }
  }
