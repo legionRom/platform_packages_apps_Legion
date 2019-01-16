79a6d01fc4611716ad42235e8df4a0be36cfd4d3
Cristiano Matos <cristianobmatos@gmail.com>
2019-01-16 15:28:11 +0100

    RecentsUI: recents panel clear all button tweaks
    * Show or hide recents button
    * Place the button in top left, center, right or bottom left, center, right

--- src/com/beast/settings/fragments/RecentsSettings.java
+++ src/com/beast/settings/fragments/RecentsSettings.java
@@ -23,8 +23,12 @@ import com.beast.settings.preferences.Utils;
  public class RecentsSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 			
-    private ListPreference mRecentsComponentType;
     private static final String RECENTS_COMPONENT_TYPE = "recents_component";
+    private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
+    private ListPreference mRecentsComponentType;
+    private ListPreference mRecentsClearAllLocation;
+    private SwitchPreference mRecentsClearAll;
 			
 			
      @Override
@@ -40,6 +44,14 @@ import com.beast.settings.preferences.Utils;
         mRecentsComponentType.setValue(String.valueOf(type));
         mRecentsComponentType.setSummary(mRecentsComponentType.getEntry());
         mRecentsComponentType.setOnPreferenceChangeListener(this);
+		
+        // clear all recents
+        mRecentsClearAllLocation = (ListPreference) findPreference(RECENTS_CLEAR_ALL_LOCATION);
+        int location = Settings.System.getIntForUser(resolver,
+                Settings.System.RECENTS_CLEAR_ALL_LOCATION, 3, UserHandle.USER_CURRENT);
+        mRecentsClearAllLocation.setValue(String.valueOf(location));
+        mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntry());
+        mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
      }
 
      @Override
@@ -55,6 +67,13 @@ import com.beast.settings.preferences.Utils;
                     Settings.Secure.SWIPE_UP_TO_SWITCH_APPS_ENABLED, 0);
             }
             Utils.restartSystemUi(getContext());
+        return true;
+        } else if (preference == mRecentsClearAllLocation) {
+            int location = Integer.valueOf((String) objValue);
+            int index = mRecentsClearAllLocation.findIndexOfValue((String) objValue);
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    Settings.System.RECENTS_CLEAR_ALL_LOCATION, location, UserHandle.USER_CURRENT);
+            mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntries()[index]);
         return true;
         }
      return false;
