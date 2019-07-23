BeastRoms/platform_packages_apps_Beast

Stock Recents/Overview icon pack support [3/3]
Signed-off-by: mdeejay <mdjrussia@gmail.com>

src/com/beast/settings/fragments/RecentsSettings.java
@@ -1,11 +1,35 @@
+/*
+ *  Copyright (C) 2019 BeastROMs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package com.beast.settings.fragments;
- import com.android.internal.logging.nano.MetricsProto;
-import android.app.Activity;
-import android.content.Context;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
@@ -13,19 +37,52 @@ import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ListView;
+
+import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
-import java.util.Arrays;
-import java.util.HashSet;
 import com.android.settings.SettingsPreferenceFragment;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
 import com.beast.settings.preferences.Utils;
 
 
  public class RecentsSettings extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
+        OnPreferenceChangeListener, DialogInterface.OnDismissListener {
 			
     private static final String RECENTS_COMPONENT_TYPE = "recents_component";
     private static final String RECENTS_TYPE = "recents_layout_style";
     private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+	
+    private final static String[] sSupportedActions = new String[] {
+        "org.adw.launcher.THEMES",
+        "com.gau.go.launcherex.theme"
+    };
+
+    private static final String[] sSupportedCategories = new String[] {
+        "com.fede.launcher.THEME_ICONPACK",
+        "com.anddoes.launcher.THEME",
+        "com.teslacoilsw.launcher.THEME"
+    };
+
+    private AlertDialog mDialog;
+    private ListView mListView;
 
     private ListPreference mRecentsComponentType;
     private ListPreference mRecentsClearAllLocation;
@@ -34,8 +91,9 @@ import com.beast.settings.preferences.Utils;
 			
 			
      @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
          addPreferencesFromResource(R.xml.beast_settings_recents);
          ContentResolver resolver = getActivity().getContentResolver();
 
@@ -63,6 +121,11 @@ import com.beast.settings.preferences.Utils;
         mRecentsType.setSummary(mRecentsType.getEntry());
         mRecentsType.setOnPreferenceChangeListener(this);
      }
+	 
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.BEAST;
+    }
 
      @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
@@ -98,7 +161,168 @@ import com.beast.settings.preferences.Utils;
      }
 
      @Override
-    public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.BEAST;
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference == findPreference("recents_icon_pack")) {
+            pickIconPack(getContext());
+            return true;
+        }
+        return super.onPreferenceTreeClick(preference);
+    }
+
+    /** Recents Icon Pack Dialog **/
+    private void pickIconPack(final Context context) {
+        if (mDialog != null) {
+            return;
+        }
+        Map<String, IconPackInfo> supportedPackages = getSupportedPackages(context);
+        if (supportedPackages.isEmpty()) {
+            Toast.makeText(context, R.string.no_iconpacks_summary, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+        .setTitle(R.string.dialog_pick_iconpack_title)
+        .setOnDismissListener(this)
+        .setNegativeButton(R.string.cancel, null)
+        .setView(createDialogView(context, supportedPackages));
+        mDialog = builder.show();
+    }
+
+    private View createDialogView(final Context context, Map<String, IconPackInfo> supportedPackages) {
+        final LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View view = inflater.inflate(R.layout.dialog_iconpack, null);
+        final IconAdapter adapter = new IconAdapter(context, supportedPackages);
+
+        mListView = (ListView) view.findViewById(R.id.iconpack_list);
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                        int position, long id) {
+                if (adapter.isCurrentIconPack(position)) {
+                    return;
+                }
+                String selectedPackage = adapter.getItem(position);
+                Settings.System.putString(getContext().getContentResolver(),
+                        Settings.System.RECENTS_ICON_PACK, selectedPackage);
+                mDialog.dismiss();
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        if (mDialog != null) {
+            mDialog = null;
+        }
+    }
+
+    private static class IconAdapter extends BaseAdapter {
+        ArrayList<IconPackInfo> mSupportedPackages;
+        LayoutInflater mLayoutInflater;
+        String mCurrentIconPack;
+        int mCurrentIconPackPosition = -1;
+
+        IconAdapter(Context ctx, Map<String, IconPackInfo> supportedPackages) {
+            mLayoutInflater = LayoutInflater.from(ctx);
+            mSupportedPackages = new ArrayList<IconPackInfo>(supportedPackages.values());
+            Collections.sort(mSupportedPackages, new Comparator<IconPackInfo>() {
+                @Override
+                public int compare(IconPackInfo lhs, IconPackInfo rhs) {
+                    return lhs.label.toString().compareToIgnoreCase(rhs.label.toString());
+                }
+            });
+
+            Resources res = ctx.getResources();
+            String defaultLabel = res.getString(R.string.default_iconpack_title);
+            Drawable icon = res.getDrawable(android.R.drawable.sym_def_app_icon);
+            mSupportedPackages.add(0, new IconPackInfo(defaultLabel, icon, ""));
+            mCurrentIconPack = Settings.System.getString(ctx.getContentResolver(),
+                Settings.System.RECENTS_ICON_PACK);
+        }
+
+        @Override
+        public int getCount() {
+            return mSupportedPackages.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return (String) mSupportedPackages.get(position).packageName;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        public boolean isCurrentIconPack(int position) {
+            return mCurrentIconPackPosition == position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = mLayoutInflater.inflate(R.layout.iconpack_view_radio, null);
+            }
+            IconPackInfo info = mSupportedPackages.get(position);
+            TextView txtView = (TextView) convertView.findViewById(R.id.title);
+            txtView.setText(info.label);
+            ImageView imgView = (ImageView) convertView.findViewById(R.id.icon);
+            imgView.setImageDrawable(info.icon);
+            RadioButton radioButton = (RadioButton) convertView.findViewById(R.id.radio);
+            boolean isCurrentIconPack = info.packageName.equals(mCurrentIconPack);
+            radioButton.setChecked(isCurrentIconPack);
+            if (isCurrentIconPack) {
+                mCurrentIconPackPosition = position;
+            }
+            return convertView;
+        }
+    }
+
+    private Map<String, IconPackInfo> getSupportedPackages(Context context) {
+        Intent i = new Intent();
+        Map<String, IconPackInfo> packages = new HashMap<String, IconPackInfo>();
+        PackageManager packageManager = context.getPackageManager();
+        for (String action : sSupportedActions) {
+            i.setAction(action);
+            for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
+                IconPackInfo info = new IconPackInfo(r, packageManager);
+                packages.put(r.activityInfo.packageName, info);
+            }
+        }
+        i = new Intent(Intent.ACTION_MAIN);
+        for (String category : sSupportedCategories) {
+            i.addCategory(category);
+            for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
+                IconPackInfo info = new IconPackInfo(r, packageManager);
+                packages.put(r.activityInfo.packageName, info);
+            }
+            i.removeCategory(category);
+        }
+        return packages;
+    }
+
+    static class IconPackInfo {
+        String packageName;
+        CharSequence label;
+        Drawable icon;
+
+        IconPackInfo(ResolveInfo r, PackageManager packageManager) {
+            packageName = r.activityInfo.packageName;
+            icon = r.loadIcon(packageManager);
+            label = r.loadLabel(packageManager);
+        }
+
+        IconPackInfo(){
+        }
+
+        public IconPackInfo(String label, Drawable icon, String packageName) {
+            this.label = label;
+            this.icon = icon;
+            this.packageName = packageName;
+        }
     }
 }

legionRom/platform_packages_apps_Legion

src/com/legion/settings/fragments/RecentsSettings.java
@@ -1,11 +1,17 @@
 package com.legion.settings.fragments;
- import com.android.internal.logging.nano.MetricsProto;
-import android.app.Activity;
-import android.content.Context;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
@@ -13,18 +19,49 @@ import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ListView;
+
+import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
-import java.util.Arrays;
-import java.util.HashSet;
 import com.android.settings.SettingsPreferenceFragment;
 import com.legion.settings.preferences.Utils;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
 
 
  public class RecentsSettings extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
+        OnPreferenceChangeListener, DialogInterface.OnDismissListener {
 			
     private static final String RECENTS_COMPONENT_TYPE = "recents_component";
     private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+	
+    private final static String[] sSupportedActions = new String[] {
+        "org.adw.launcher.THEMES",
+        "com.gau.go.launcherex.theme"
+    };
+
+    private static final String[] sSupportedCategories = new String[] {
+        "com.fede.launcher.THEME_ICONPACK",
+        "com.anddoes.launcher.THEME",
+        "com.teslacoilsw.launcher.THEME"
+    };
+
+    private AlertDialog mDialog;
+    private ListView mListView;
 
     private ListPreference mRecentsComponentType;
     private ListPreference mRecentsClearAllLocation;
@@ -32,8 +69,9 @@ import com.legion.settings.preferences.Utils;
 			
 			
      @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
          addPreferencesFromResource(R.xml.legion_settings_recents);
          ContentResolver resolver = getActivity().getContentResolver();
 
@@ -54,6 +92,11 @@ import com.legion.settings.preferences.Utils;
         mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
 
      }
+	 
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.LEGION;
+    }
 
      @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
@@ -81,7 +124,168 @@ import com.legion.settings.preferences.Utils;
      }
 
      @Override
-    public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.LEGION;
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference == findPreference("recents_icon_pack")) {
+            pickIconPack(getContext());
+            return true;
+        }
+        return super.onPreferenceTreeClick(preference);
+    }
+
+    /** Recents Icon Pack Dialog **/
+    private void pickIconPack(final Context context) {
+        if (mDialog != null) {
+            return;
+        }
+        Map<String, IconPackInfo> supportedPackages = getSupportedPackages(context);
+        if (supportedPackages.isEmpty()) {
+            Toast.makeText(context, R.string.no_iconpacks_summary, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+        .setTitle(R.string.dialog_pick_iconpack_title)
+        .setOnDismissListener(this)
+        .setNegativeButton(R.string.cancel, null)
+        .setView(createDialogView(context, supportedPackages));
+        mDialog = builder.show();
+    }
+
+    private View createDialogView(final Context context, Map<String, IconPackInfo> supportedPackages) {
+        final LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View view = inflater.inflate(R.layout.dialog_iconpack, null);
+        final IconAdapter adapter = new IconAdapter(context, supportedPackages);
+
+        mListView = (ListView) view.findViewById(R.id.iconpack_list);
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                        int position, long id) {
+                if (adapter.isCurrentIconPack(position)) {
+                    return;
+                }
+                String selectedPackage = adapter.getItem(position);
+                Settings.System.putString(getContext().getContentResolver(),
+                        Settings.System.RECENTS_ICON_PACK, selectedPackage);
+                mDialog.dismiss();
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        if (mDialog != null) {
+            mDialog = null;
+        }
+    }
+
+    private static class IconAdapter extends BaseAdapter {
+        ArrayList<IconPackInfo> mSupportedPackages;
+        LayoutInflater mLayoutInflater;
+        String mCurrentIconPack;
+        int mCurrentIconPackPosition = -1;
+
+        IconAdapter(Context ctx, Map<String, IconPackInfo> supportedPackages) {
+            mLayoutInflater = LayoutInflater.from(ctx);
+            mSupportedPackages = new ArrayList<IconPackInfo>(supportedPackages.values());
+            Collections.sort(mSupportedPackages, new Comparator<IconPackInfo>() {
+                @Override
+                public int compare(IconPackInfo lhs, IconPackInfo rhs) {
+                    return lhs.label.toString().compareToIgnoreCase(rhs.label.toString());
+                }
+            });
+
+            Resources res = ctx.getResources();
+            String defaultLabel = res.getString(R.string.default_iconpack_title);
+            Drawable icon = res.getDrawable(android.R.drawable.sym_def_app_icon);
+            mSupportedPackages.add(0, new IconPackInfo(defaultLabel, icon, ""));
+            mCurrentIconPack = Settings.System.getString(ctx.getContentResolver(),
+                Settings.System.RECENTS_ICON_PACK);
+        }
+
+        @Override
+        public int getCount() {
+            return mSupportedPackages.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return (String) mSupportedPackages.get(position).packageName;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        public boolean isCurrentIconPack(int position) {
+            return mCurrentIconPackPosition == position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = mLayoutInflater.inflate(R.layout.iconpack_view_radio, null);
+            }
+            IconPackInfo info = mSupportedPackages.get(position);
+            TextView txtView = (TextView) convertView.findViewById(R.id.title);
+            txtView.setText(info.label);
+            ImageView imgView = (ImageView) convertView.findViewById(R.id.icon);
+            imgView.setImageDrawable(info.icon);
+            RadioButton radioButton = (RadioButton) convertView.findViewById(R.id.radio);
+            boolean isCurrentIconPack = info.packageName.equals(mCurrentIconPack);
+            radioButton.setChecked(isCurrentIconPack);
+            if (isCurrentIconPack) {
+                mCurrentIconPackPosition = position;
+            }
+            return convertView;
+        }
+    }
+
+    private Map<String, IconPackInfo> getSupportedPackages(Context context) {
+        Intent i = new Intent();
+        Map<String, IconPackInfo> packages = new HashMap<String, IconPackInfo>();
+        PackageManager packageManager = context.getPackageManager();
+        for (String action : sSupportedActions) {
+            i.setAction(action);
+            for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
+                IconPackInfo info = new IconPackInfo(r, packageManager);
+                packages.put(r.activityInfo.packageName, info);
+            }
+        }
+        i = new Intent(Intent.ACTION_MAIN);
+        for (String category : sSupportedCategories) {
+            i.addCategory(category);
+            for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
+                IconPackInfo info = new IconPackInfo(r, packageManager);
+                packages.put(r.activityInfo.packageName, info);
+            }
+            i.removeCategory(category);
+        }
+        return packages;
+    }
+
+    static class IconPackInfo {
+        String packageName;
+        CharSequence label;
+        Drawable icon;
+
+        IconPackInfo(ResolveInfo r, PackageManager packageManager) {
+            packageName = r.activityInfo.packageName;
+            icon = r.loadIcon(packageManager);
+            label = r.loadLabel(packageManager);
+        }
+
+        IconPackInfo(){
+        }
+
+        public IconPackInfo(String label, Drawable icon, String packageName) {
+            this.label = label;
+            this.icon = icon;
+            this.packageName = packageName;
+       }
     }
 }