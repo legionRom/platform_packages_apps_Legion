2f8c8f18dd5c01edf71ba4f996b194afe6f86022
Anushek Prasal <anushekprasal@gmail.com>
2019-10-29 15:52:36 +0530

    ConfigCenter: Add LED lights settings

--- src/com/legion/settings/NotificationsSettings.java
+++ src/com/legion/settings/NotificationsSettings.java
@@ -27,11 +27,17 @@ import com.android.internal.logging.nano.MetricsProto;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.legion.settings.preference.SystemSettingMasterSwitchPreference;
 
 public class NotificationsSettings extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
     public static final String TAG = "NotificationsSettings";
+    private static final String LIGHTS_CATEGORY = "notification_lights";
+    private static final String BATTERY_LIGHT_ENABLED = "battery_light_enabled";
+
+    private PreferenceCategory mLightsCategory;
+    private SystemSettingMasterSwitchPreference mBatteryLightEnabled;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,10 +45,27 @@ public class NotificationsSettings extends SettingsPreferenceFragment
         addPreferencesFromResource(R.xml.notifications_settings);
         PreferenceScreen prefScreen = getPreferenceScreen();
         final Resources res = getResources();
+
+        mBatteryLightEnabled = (SystemSettingMasterSwitchPreference) findPreference(BATTERY_LIGHT_ENABLED);
+        mBatteryLightEnabled.setOnPreferenceChangeListener(this);
+        int batteryLightEnabled = Settings.System.getInt(getContentResolver(),
+                BATTERY_LIGHT_ENABLED, 1);
+        mBatteryLightEnabled.setChecked(batteryLightEnabled != 0);
+
+        mLightsCategory = (PreferenceCategory) findPreference(LIGHTS_CATEGORY);
+        if (!getResources().getBoolean(com.android.internal.R.bool.config_hasNotificationLed)) {
+            getPreferenceScreen().removePreference(mLightsCategory);
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mBatteryLightEnabled) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getContentResolver(),
+		            BATTERY_LIGHT_ENABLED, value ? 1 : 0);
+            return true;
+	}
         return false;
     }
 
