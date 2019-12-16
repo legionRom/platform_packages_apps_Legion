50512dffcfe68d46555cdc00bd8a24131a955829
rajkale99 <kaleraj.rk@gmail.com>
2019-12-16 23:52:19 +0530

    add Themepicker into legion settings

--- src/com/legion/settings/ThemesSettings.java
+++ src/com/legion/settings/ThemesSettings.java
@@ -9,12 +9,24 @@ import com.android.settings.SettingsPreferenceFragment;
 
 public class ThemesSettings extends SettingsPreferenceFragment {
 
+    private static final String CUSTOM_THEME_BROWSE = "theme_select_activity";
+    private Preference mThemeBrowse;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.themes_settings);
+	addPreferencesFromResource(R.xml.themes_settings);
+	mThemeBrowse = findPreference(CUSTOM_THEME_BROWSE);
+        mThemeBrowse.setEnabled(isBrowseThemesAvailable());
+
     }
+
+    private boolean isBrowseThemesAvailable() {
+        PackageManager pm = getPackageManager();
+	Intent browse = new Intent();
+	browse.setClassName("com.android.customization", "com.android.customization.picker.CustomizationPickerActivity");
+        return pm.resolveActivity(browse, 0) != null;
 
     @Override
     public int getMetricsCategory() {
