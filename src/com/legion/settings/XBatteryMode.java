Project-Xtended/packages_apps_Xtensions

Add battery styles and customizations for Android 10 [2/2]

src/com/msm/xtended/fragments/XBatteryMode.java
@@ -1,0 +1,52 @@
+package com.msm.xtended.fragments;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+import android.provider.Settings;
+import com.android.settings.R;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+public class XBatteryMode extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        addPreferencesFromResource(R.xml.x_battery_mode);
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.XTENSIONS;
+    }
+}
+
+

legionRom/platform_packages_apps_Legion

src/com/legion/settings/XBatteryMode.java
@@ -1,0 +1,52 @@
+package com.legion.settings;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+import android.provider.Settings;
+import com.android.settings.R;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+public class XBatteryMode extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        addPreferencesFromResource(R.xml.x_battery_mode);
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.LEGION_SETTINGS;
+    }
+}
+
+