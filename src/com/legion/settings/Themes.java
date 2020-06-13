971a967975535329409d882e3ca4b5537db316c7
rajkale99 <kaleraj.rk@gmail.com>
2020-06-13 10:34:02 +0000

    Add Reset Drawable with string

--- src/com/legion/settings/Themes.java
+++ src/com/legion/settings/Themes.java
@@ -224,8 +224,8 @@ public class Themes extends SettingsPreferenceFragment implements
      }
 
     private void getQsHeaderStylePref() {
-        mQsHeaderStyle = (ListPreference) screen.findPreference(QS_HEADER_STYLE);
-        int qsHeaderStyle = Settings.System.getInt(getActivity.getContentResolver(),
+        mQsHeaderStyle = (ListPreference) findPreference(QS_HEADER_STYLE);
+        int qsHeaderStyle = Settings.System.getInt(getActivity().getContentResolver(),
                 Settings.System.QS_HEADER_STYLE, 0);
         int valueIndex = mQsHeaderStyle.findIndexOfValue(String.valueOf(qsHeaderStyle));
         mQsHeaderStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
